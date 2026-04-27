b6d509e027f57495ea61117df78ce2cf626d7a92
maaroufighofrane398-stack <maaroufighofrane398@gmail.com>
2026-04-27 05:56:01 +0100

    Create bot.py
    import os
    from telegram import Update
    from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
    
    TOKEN = os.getenv("TOKEN")
    
    async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
        await update.message.reply_text("أهلا 👋\nتم تشغيل البوت بنجاح")
    
    async def main():
        app = ApplicationBuilder().token(TOKEN).build()
        app.add_handler(CommandHandler("start", start))
        await app.run_polling()
    
    if __name__ == "__main__":
        import asyncio
        print("Bot running...")
        asyncio.run(main())

---
+++ bot.py
@@ -0,0 +1,18 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("أهلا 👋\nتم تشغيل البوت بنجاح")
+
+async def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    await app.run_polling()
+
+if __name__ == "__main__":
+    import asyncio
+    print("Bot running...")
+    asyncio.run(main())
